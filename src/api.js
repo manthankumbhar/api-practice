cddafc511c5960d3731e0e048ab409432756adaa
Manthan Kumbhar <kum.manthan@gmail.com>
2021-02-04 23:50:34 +0530

    api debug

--- src/api.js
+++ src/api.js
@@ -15,16 +15,19 @@ router.post("/", (req, res) => {
   console.log("passed");
   let data = JSON.parse(req.body);
   console.log("passed");
-  request.post({
-    url:
-      "https://discord.com/api/webhooks/804824333913423923/qlEpAryvDXGPrXkIHrBI5JDmOMbi6B8u8bg_pL8sMVMBhhrt5o8sk37JQYGSwOc3l01k",
-    headers: {
-      "Content-Type": "application/json",
+  request.post(
+    {
+      url:
+        "https://discord.com/api/webhooks/804824333913423923/qlEpAryvDXGPrXkIHrBI5JDmOMbi6B8u8bg_pL8sMVMBhhrt5o8sk37JQYGSwOc3l01k",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      json: {
+        content: `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`,
+      },
     },
-    json: {
-      content: `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`,
-    },
-  });
+    console.log(request.post ? true : false)
+  );
   console.log("passed");
   res.json(
     `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`
