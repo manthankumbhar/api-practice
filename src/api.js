788947af7c7ae9c091bbdc1c63a978ec006ecffb
Manthan Kumbhar <kum.manthan@gmail.com>
2021-02-05 04:01:51 +0530

    8

--- src/api.js
+++ src/api.js
@@ -2,19 +2,20 @@ const { Router, json } = require("express");
 const express = require("express");
 const serverless = require("serverless-http");
 const request = require("request");
+const axios = require("axios");
 
 const app = express();
 
 const router = express.Router();
+
+//<------------------------------------------------------------------------------------------------------------------------------------------------>
 
 router.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
 router.post("/", (req, res) => {
-  console.log("passed");
   let data = JSON.parse(req.body);
-  console.log("passed");
   request.post({
     url:
       "https://discord.com/api/webhooks/804824333913423923/qlEpAryvDXGPrXkIHrBI5JDmOMbi6B8u8bg_pL8sMVMBhhrt5o8sk37JQYGSwOc3l01k",
@@ -25,12 +26,12 @@ router.post("/", (req, res) => {
       content: `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`,
     },
   });
-  console.log("passed");
   res.json(
     `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`
   );
-  console.log("passed");
 });
+
+//<------------------------------------------------------------------------------------------------------------------------------------------------>
 
 router.get("/dummy_discord", (req, res) => {
   res.json({ message: "hello" });
@@ -49,6 +50,27 @@ router.post("/dummy_discord", (req, res) => {
   });
   res.json({ message: "Done" });
 });
+
+//<------------------------------------------------------------------------------------------------------------------------------------------------>
+
+router.get("/test", (req, res) => {
+  res.json({ message: "hello" });
+});
+
+router.post("/test", (req, res) => {
+  // let data = JSON.parse(req.body);
+  axios({
+    method: "post",
+    url:
+      "https://discord.com/api/webhooks/807014769335992320/_wsZUCx1ck_APvuj1zBTzs4c4tlKF_JIbF5porzpWLE7hNvGpG1YwbFHgaDqEitU7LXm",
+    data: {
+      content: "hello",
+    },
+  });
+  res.json({ message: "Done" });
+});
+
+//<------------------------------------------------------------------------------------------------------------------------------------------------>
 
 app.use("/.netlify/functions/api", router);
 
