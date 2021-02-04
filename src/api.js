4198a32e1694da9d72c622632f9cbf7bd0a81bbc
Manthan Kumbhar <kum.manthan@gmail.com>
2021-02-04 07:44:22 +0530

    discord connected

--- src/api.js
+++ src/api.js
@@ -1,20 +1,47 @@
 const { Router, json } = require("express");
 const express = require("express");
 const serverless = require("serverless-http");
+const request = require("request");
 
 const app = express();
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json(req.body);
+  res.json({ message: "hello" });
 });
 
 router.post("/", (req, res) => {
   let data = JSON.parse(req.body);
+  request.post({
+    url:
+      "https://discord.com/api/webhooks/804824333913423923/qlEpAryvDXGPrXkIHrBI5JDmOMbi6B8u8bg_pL8sMVMBhhrt5o8sk37JQYGSwOc3l01k",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    json: {
+      content: `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`,
+    },
+  });
   res.json(
     `${data.repository.owner.name} just pushed a commit with message - '${data.head_commit.message}' to <${data.repository.name}>`
   );
+});
+
+router.get("/dummy_discord", (req, res) => {
+  res.json({ message: "hello" });
+});
+
+router.post("/dummy_discord", (req, res) => {
+  request.post({
+    url:
+      "https://discord.com/api/webhooks/804824333913423923/qlEpAryvDXGPrXkIHrBI5JDmOMbi6B8u8bg_pL8sMVMBhhrt5o8sk37JQYGSwOc3l01k",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    json: { content: "hello" },
+  });
+  res.json({ content: "hola" });
 });
 
 app.use("/.netlify/functions/api", router);
