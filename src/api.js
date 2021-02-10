e4acdae972ad4e65785b92c1b4a1f3ea911b7f05
Manthan Kumbhar <kum.manthan@gmail.com>
2021-02-10 05:52:19 +0530

    error handling

--- src/api.js
+++ src/api.js
@@ -8,23 +8,26 @@ const app = express();
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/ready", (req, res) => {
   res.json({ message: "hello" });
 });
 
-router.post("/", async (req, res) => {
-  let reqData = JSON.parse(req.body);
-  await axios({
-    method: "post",
-    url: process.env.DISCORD_WEBHOOK_URL || DISCORD_WEBHOOK_URL,
-    data: {
-      content: JSON.stringify(
-        `${reqData.repository.owner.login} just pushed a commit with message - '${reqData.head_commit.message}' to <${reqData.repository.name}>`
-      ),
-      ContentType: "application/json",
-    },
-  });
-  res.json({ message: "done" });
+router.post("/ready", async (req, res, error) => {
+  try {
+    let reqData = JSON.parse(req.body);
+    const message = `${reqData.repository.owner.login} just pushed a commit with message - '${reqData.head_commit.message}' to <${reqData.repository.name}>`;
+    await axios({
+      method: "post",
+      url: process.env.DISCORD_WEBHOOK_URL || DISCORD_WEBHOOK_URL,
+      data: {
+        content: JSON.stringify(message),
+        ContentType: "application/json",
+      },
+    });
+    res.json({ message: "success" });
+  } catch (error) {
+    res.status(400).json({ error: "bad format" });
+  }
 });
 
 app.use("/.netlify/functions/api", router);
