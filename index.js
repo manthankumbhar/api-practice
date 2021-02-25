4e19645c9069b24a9b299a4e0743f38f4a958931
Manthan Kumbhar <kum.manthan@gmail.com>
2021-02-25 23:15:01 +0530

    function addition

--- index.js
+++ index.js
@@ -20,29 +20,35 @@ app.post("/github_push_webhook/:id", async (req, res) => {
     if (reqData.head_commit === undefined) {
       return res.status(200).json({ message: "Github is testing" });
     }
+
     if (
       reqData.repository.owner.login === null ||
       reqData.repository.name === null
     ) {
       return res.status(400).json({ error: "bad format" });
     }
-    var message = `${reqData.repository.owner.login} just pushed a commit with message - '${reqData.head_commit.message}' to <${reqData.repository.name}>`;
-    const data = await pool.query(
-      "select discord_url from github_discord_url where id = $1",
-      [id]
-    );
-    const github_discord_url = data.rows[0];
 
-    console.log(data.rows[0]);
+    async function discord_post_message() {
+      const data = await pool.query(
+        "select discord_url from github_discord_url where id = $1",
+        [id]
+      );
 
-    await axios({
-      method: "POST",
-      url: github_discord_url["discord_url"],
-      data: {
-        content: JSON.stringify(message),
-        ContentType: "application/json",
-      },
-    });
+      const github_discord_url = data.rows[0];
+
+      var message = `${reqData.repository.owner.login} just pushed a commit with message - '${reqData.head_commit.message}' to <${reqData.repository.name}>`;
+
+      await axios({
+        method: "POST",
+        url: github_discord_url["discord_url"],
+        data: {
+          content: JSON.stringify(message),
+          ContentType: "application/json",
+        },
+      });
+    }
+    discord_post_message();
+
     res.status(200).json("Success");
   } catch (err) {
     res.status(400).json(err.message);
