2a20cb78eb00dd86995244a9178b562ee59b9efa
Manthan Kumbhar <kum.manthan@gmail.com>
2021-02-27 04:36:05 +0530

    reusable function bug cleared - 1

--- index.js
+++ index.js
@@ -10,35 +10,19 @@ app.use(cors());
 
 let port = process.env.PORT || 3000;
 
-async function universal_api_posts() {
-  try {
-    const data_1 = await pool.query(
-      `select discord_url from github_discord_url where id = $1`,
-      [`${db_id}`]
-    );
-    // console.log("ok");
-    // console.log(data_1.rows[0]);
+async function get_github_discord_url_by_id() {
+  const data_1 = await pool.query(
+    `select discord_url from github_discord_url where id = $1`,
+    [`${db_id}`]
+  );
 
-    const github_discord_url = data_1.rows[0];
-    // console.log(github_discord_url);
-
-    if (data_1.rows === null || data_1.rows >= 2) {
-      throw new Error("row doesn't exist");
-    }
-
-    var message = `${param1} just pushed a commit with message - '${param2}' to <${param3}>`;
-
-    await axios({
-      method: "POST",
-      url: github_discord_url[`${db_url}`],
-      data: {
-        content: JSON.stringify(message),
-        ContentType: "application/json",
-      },
-    });
-  } catch (err) {
-    console.log(err.message);
+  if (data_1.rows === null || data_1.rows >= 2 || data_1.rows.length == 0) {
+    throw new Error("row doesn't exist");
   }
+
+  const github_discord_url = await data_1.rows[0];
+
+  return github_discord_url["discord_url"];
 }
 app.listen(port, () => {
   console.log(`running on ${port}`);
@@ -48,7 +32,6 @@ app.post("/github_push_webhook/:id", async (req, res) => {
   try {
     const { id } = req.params;
     var reqData = req.body;
-    // console.log(id);
 
     if (reqData.head_commit === undefined) {
       return res.status(200).json({ message: "Github is testing" });
@@ -61,13 +44,20 @@ app.post("/github_push_webhook/:id", async (req, res) => {
       return res.status(400).json({ error: "bad format" });
     }
 
-    universal_api_posts(
-      ((db_id = id),
-      (param1 = reqData.repository.owner.login),
-      (param2 = reqData.head_commit.message),
-      (param3 = reqData.repository.name),
-      (db_url = "discord_url"))
-    );
+    get_github_discord_url_by_id((db_id = id));
+
+    const github_discord_url = await get_github_discord_url_by_id();
+
+    var message = `${reqData.repository.owner.login} just pushed a commit with message - '${reqData.head_commit.message}' to <${reqData.repository.name}>`;
+
+    await axios({
+      method: "POST",
+      url: github_discord_url,
+      data: {
+        content: JSON.stringify(message),
+        ContentType: "application/json",
+      },
+    });
 
     res.status(200).json("Success");
   } catch (err) {
