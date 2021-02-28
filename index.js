e4b4760bd2c901c12ee3a22def41ecdada5626c8
Manthan Kumbhar <kum.manthan@gmail.com>
2021-03-01 05:14:46 +0530

    initial api-2 changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const cors = require("cors");
 const pool = require("./database");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 
 let port = process.env.PORT || 3000;
 
@@ -55,6 +57,30 @@ app.post("/github_push_webhook/:id", async (req, res) => {
     res.status(200).json("Success");
   } catch (err) {
     res.status(400).json(err.message);
+  }
+});
+
+app.post("/github_discord_urls", async (req, res) => {
+  try {
+    const reqBody = req.body;
+    const data = await pool.query(
+      `insert into github_discord_url (discord_url) values ($1)`,
+      [reqBody.url]
+    );
+
+    const id = await pool.query(
+      `select id from github_discord_url where discord_url = $1`,
+      [reqBody.url]
+    );
+
+    const id_matching_github_and_discord = id.rows[0];
+    res
+      .status(200)
+      .json(
+        `https://v2-github-discord-api-and-me.herokuapp.com/github_push_webhook/${id_matching_github_and_discord["id"]}`
+      );
+  } catch (err) {
+    res.status(500).json(err.message);
   }
 });
 
