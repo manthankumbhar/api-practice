3b42b820c13c3155683fcffd25248cf0d2787d43
Manthan Kumbhar <kum.manthan@gmail.com>
2021-03-03 01:02:33 +0530

    api_2 added

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const cors = require("cors");
 const pool = require("./database");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 
 let port = process.env.PORT || 3000;
 
 async function get_github_discord_url_by_id() {
   const data_1 = await pool.query(
     `select discord_url from github_discord_url where id = $1`,
-    [`${db_id}`]
+    [db_id]
   );
 
   if (data_1.rows === null || data_1.rows >= 2 || data_1.rows.length == 0) {
@@ -55,6 +57,44 @@ app.post("/github_push_webhook/:id", async (req, res) => {
     res.status(200).json("Success");
   } catch (err) {
     res.status(400).json(err.message);
+  }
+});
+
+async function insert_discord_url() {
+  const data_2 = await pool.query(
+    `insert into github_discord_url (discord_url) values ($1)`,
+    [db_url]
+  );
+  return await get_db_data_by_discord_url();
+}
+
+async function get_db_data_by_discord_url() {
+  const data_3 = await pool.query(
+    `select * from github_discord_url where discord_url = $1`,
+    [db_url]
+  );
+  return data_3.rows[0];
+}
+
+async function get_github_url() {
+  const res_url = `https://v2-github-discord-api-and-me.herokuapp.com/github_push_webhook/${id}`;
+  return res_url;
+}
+
+app.post("/github_discord_urls", async (req, res) => {
+  try {
+    const reqBody = req.body;
+    if (reqBody.url == "" || reqBody.url == null) {
+      return res.status(400).json({ error: "No url entered" });
+    }
+
+    await insert_discord_url((db_url = reqBody.url));
+    const id_from_db = await get_db_data_by_discord_url((db_url = reqBody.url));
+    const res_url = await get_github_url((id = id_from_db["id"]));
+
+    res.status(200).json(res_url);
+  } catch (err) {
+    res.status(500).json(err.message);
   }
 });
 
