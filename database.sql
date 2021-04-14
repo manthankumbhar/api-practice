b8347ffbb93ed37c0414dbc574a4004fab5a0065
Manthan Kumbhar <kum.manthan@gmail.com>
2021-04-14 12:49:37 +0530

    v3.0 added

--- database.sql
+++ database.sql
@@ -4,3 +4,11 @@ create table github_discord_url(
     updated_at timestamptz DEFAULT Now() not null,
     discord_url varchar(255) not null UNIQUE
 );
+
+create table auth_user_info(
+    id uuid DEFAULT uuid_generate_v4() not null,
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    created_at timestamptz DEFAULT Now() not null,
+    updated_at timestamptz DEFAULT Now() not null
+);
